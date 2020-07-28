c63979a5e673fb76ad3be3352d1381a011b60c7d
Harsha <harshavardhanr.993@gmail.com>
2020-07-28 13:04:18 -0500

    Updated simulate.sh to redirect outputs to respective folders to outputs/<simulation-method>/output<input-file-No.>.txt

--- simulate.sh
+++ simulate.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+rm -rf outputs/*
+mkdir -p outputs/{wound-wait,wait-die}
 for method in {"wound-wait","wait-die"}; do
-    for i in $(ls -1 inputs/input*.txt); do
-        echo -e "Input file: $i"
-        python main.py $method ${i}
-        echo -e "\n"
+    for input in $(ls -1 inputs/input*.txt); do
+        output="outputs/$method/$(sed 's/inputs\///g;s/input/output/g' <<< ${input})"
+        echo -e "\nInput file: $input, Output also saved to: ${output}"
+        python main.py ${method} ${input} | tee ${output}
     done
 done
